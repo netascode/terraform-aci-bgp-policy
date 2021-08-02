185dcdb92f5eff885ed04244ba55ff6445e20ec7
danischm <danischm@cisco.com>
2021-08-02 17:32:35 +0200

    Variable validation fixes

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "fabric_bgp_as" {
-  description = "Fabric BGP AS, Minimum value: 1, Maximum value: 4294967295."
+  description = "Fabric BGP AS. Minimum value: 1. Maximum value: 4294967295."
   type        = number
 
   validation {
     condition     = var.fabric_bgp_as >= 1 && var.fabric_bgp_as <= 4294967295
-    error_message = "Minimum value: 1, Maximum value: 4294967295."
+    error_message = "Minimum value: 1. Maximum value: 4294967295."
   }
 }
 
 variable "fabric_bgp_rr" {
-  description = "Fabric BGP route reflector nodes, Allowed values `node_id`: 1-4000, Allowed values `pod`: 1-255."
+  description = "List of fabric BGP route reflector nodes. Allowed values `node_id`: 1-4000. Allowed values `pod`: 1-255."
   type = list(object({
     node_id = number
     pod     = number
@@ -20,19 +20,19 @@ variable "fabric_bgp_rr" {
     condition = alltrue([
       for rr in var.fabric_bgp_rr : (rr.node_id >= 1 && rr.node_id <= 4000)
     ])
-    error_message = "Minimum value: 1, Maximum value: 4000."
+    error_message = "`node_id`: Minimum value: 1. Maximum value: 4000."
   }
 
   validation {
     condition = alltrue([
       for rr in var.fabric_bgp_rr : (rr.pod >= 1 && rr.pod <= 255)
     ])
-    error_message = "Minimum value: 1, Maximum value: 255."
+    error_message = "`pod`: Minimum value: 1. Maximum value: 255."
   }
 }
 
 variable "fabric_bgp_external_rr" {
-  description = "Fabric BGP external route reflector nodes, Allowed values `node_id`: 1-4000, Allowed values `pod`: 1-255."
+  description = "List of fabric BGP external route reflector nodes. Allowed values `node_id`: 1-4000. Allowed values `pod`: 1-255."
   type = list(object({
     node_id = number
     pod     = number
@@ -43,13 +43,13 @@ variable "fabric_bgp_external_rr" {
     condition = alltrue([
       for rr in var.fabric_bgp_external_rr : (rr.node_id >= 1 && rr.node_id <= 4000)
     ])
-    error_message = "Minimum value: 1, Maximum value: 4000."
+    error_message = "`node_id`: Minimum value: 1. Maximum value: 4000."
   }
 
   validation {
     condition = alltrue([
       for rr in var.fabric_bgp_external_rr : (rr.pod >= 1 && rr.pod <= 255)
     ])
-    error_message = "Minimum value: 1, Maximum value: 255."
+    error_message = "`pod`: Minimum value: 1. Maximum value: 255."
   }
 }
